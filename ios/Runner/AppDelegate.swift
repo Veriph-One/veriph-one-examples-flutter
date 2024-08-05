4f3a8253986bd5b8f376f49dd058f203c3aa3463
Marco Garcia <marco.garcia@veriph.one>
2024-08-05 08:38:04 -0600

    feat: ios startVerification

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,13 +1,50 @@
 import Flutter
 import UIKit
+import VeriphOne
+import SwiftUI
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
+    override func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
+        let veriphOneChannel = FlutterMethodChannel(name: "one.veriph.sdk/verification",
+                                                    binaryMessenger: controller.binaryMessenger)
+        
+        veriphOneChannel.setMethodCallHandler({
+            (call: FlutterMethodCall, result: @escaping FlutterResult) -> Void in
+            if call.method == "startVerification" {
+                guard let args = call.arguments as? [String: String],
+                      let apiKey = args["apiKey"],
+                      let sessionUuid = args["sessionUuid"] else {
+                    result(FlutterError(code: "INVALID_ARGUMENT", message: "Invalid argument(s) provided", details: nil))
+                    return
+                }
+                self.startVerification(apiKey: apiKey, sessionUuid: sessionUuid, controller: controller, result: result)
+            } else {
+                result(FlutterMethodNotImplemented)
+            }
+        })
+        
+        GeneratedPluginRegistrant.register(with: self)
+        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    }
+    
+    private func startVerification(apiKey: String, sessionUuid: String, controller: FlutterViewController, result: @escaping FlutterResult) {
+        let veriphOneView = VeriphOneView(sessionUuid: sessionUuid, apiKey: apiKey) { a in
+            if let a = a {
+                result(a)
+                DispatchQueue.main.async {
+                    controller.dismiss(animated: true)
+                }
+            } else {
+                result(FlutterError(code: "INVALID_ARGUMENT", message: "Invalid argument(s) provided", details: nil))
+            }
+        }
+        
+        let hostingController = UIHostingController(rootView: veriphOneView)
+        controller.present(hostingController, animated: true)
+    }
 }
